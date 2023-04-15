2121cdf33b652a8a160264eb9e0ad63d8e5d147d
Dmitry Pilavskiy <saw.coding@gmail.com>
2023-04-15 08:46:17 +0300

    11_lesson

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
 import React from 'react';
 import './App.css';
-import {Technologies} from "./Technologies";
-import {Header} from "./Header";
 
 const App = () => {
   return (
-    <div>
-      <Header/>
-      <Technologies/>
+    <div className="app-wrapper">
+      <header className="header">
+        <img src="https://cdn.worldvectorlogo.com/logos/react-1.svg"/>
+      </header>
+      <nav className="nav">
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+      <div className="content">
+        <div>
+          <img
+            src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+            alt="picture"/>
+        </div>
+        <div>
+          ava + description
+        </div>
+        <div>
+          My posts
+          <div>
+            New post
+          </div>
+          <div>
+            <div>
+              post 1
+            </div>
+            <div>
+              post 2
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
