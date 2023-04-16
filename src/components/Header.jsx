4e68030c30d136261304d84566c69468abfbb34d
Dmitry Pilavskiy <saw.coding@gmail.com>
2023-04-16 15:55:57 +0300

    14_lesson

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from "./Header.module.css"
 
 export const Header = () => {
   return (
-    <header className="header">
+    <header className={s.header}>
       <img src="https://cdn.worldvectorlogo.com/logos/react-1.svg" alt="logo"/>
     </header>
   )
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from "./Profile.module.css"
 
 export const Profile = () => {
   return (
-    <div className="content">
+    <div className={s.content}>
       <div>
         <img
           src="https://www.planetware.com/wpimages/2019/11/canada-in-pictures-beautiful-places-to-photograph-morraine-lake.jpg"
@@ -16,11 +17,11 @@ export const Profile = () => {
         <div>
           New post
         </div>
-        <div>
-          <div>
+        <div className={s.posts}>
+          <div className={s.item}>
             post 1
           </div>
-          <div>
+          <div className={s.item}>
             post 2
           </div>
         </div>
