d474954d5d62ed076d92b76581b50b2a8ca1e473
Dmitry Pilavskiy <saw.coding@gmail.com>
2023-04-17 13:22:35 +0300

    15_lesson

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import s from "./Profile.module.css"
+import {MyPosts} from "./MyPosts/MyPosts";
 
 export const Profile = () => {
   return (
@@ -12,20 +13,7 @@ export const Profile = () => {
       <div>
         ava + description
       </div>
-      <div>
-        My posts
-        <div>
-          New post
-        </div>
-        <div className={s.posts}>
-          <div className={s.item}>
-            post 1
-          </div>
-          <div className={s.item}>
-            post 2
-          </div>
-        </div>
-      </div>
+      <MyPosts />
     </div>
   )
 }
