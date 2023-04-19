ec90142e32df8b4c212f1b61267c3a13da83de23
Dmitry Pilavskiy <saw.coding@gmail.com>
2023-04-19 20:40:57 +0300

    26_lesson

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,10 +4,15 @@ import {MyPosts} from "./MyPosts/MyPosts";
 import {ProfileInfo} from "./ProfileInfo/ProfileInfo";
 
 export const Profile = () => {
+  let posts = [
+    {id: 1, message: "Hi, how are you?", likesCount: 12},
+    {id: 2, message: "It's my first post", likesCount: 11},
+  ]
+
   return (
     <div>
       <ProfileInfo />
-      <MyPosts />
+      <MyPosts posts={posts}/>
     </div>
   )
 }
