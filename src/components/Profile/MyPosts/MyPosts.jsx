9628a7d5fdb45773336308c9997f2a541c0cf6bf
Dmitry Pilavskiy <saw.coding@gmail.com>
2023-04-25 20:54:12 +0300

    39_lesson

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import s from "./MyPosts.module.css"
 import {Post} from "./Post/Post";
+import {addPostAC, updateNewPostTextAC} from "../../../redux/state";
+
+
 
 export const MyPosts = (props) => {
   let postsElements = props.posts.map(p => <Post key={p.id} message={p.message} likesCount={p.likesCount}/>)
@@ -8,12 +11,12 @@ export const MyPosts = (props) => {
   let newPostElement = React.createRef()
 
   let addPost = () => {
-    props.dispatch({type: "ADD-POST"})
+    props.dispatch(addPostAC())
   }
 
   let onPostChange = () => {
     let text = newPostElement.current.value
-    props.dispatch({type: "UPDATE-NEW-POST-TEXT", newText: text})
+    props.dispatch(updateNewPostTextAC(text))
   }
 
   return (
