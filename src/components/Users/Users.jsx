8f79da06544a4fda0053aeed7f1f0caf6ae62609
Dmitry Pilavskiy <saw.coding@gmail.com>
2023-05-02 10:29:03 +0300

    52_lesson

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,13 +5,18 @@ import userPhoto from "../../assets/images/default.png"
 
 const Users = (props) => {
 
-  if (props.users.length === 0) {
-    axios.get("https://social-network.samuraijs.com/api/1.0/users")
-      .then(response => {props.setUsers(response.data.items)})
+  let getUsers = () => {
+    if (props.users.length === 0) {
+      axios.get("https://social-network.samuraijs.com/api/1.0/users")
+        .then(response => {
+          props.setUsers(response.data.items)
+        })
+    }
   }
 
   return (
     <div>
+      <button onClick={getUsers}>Get Users</button>
       {props.users.map(u => <div key={u.id}>
         <span>
           <div>
@@ -19,8 +24,12 @@ const Users = (props) => {
           </div>
           <div>
             {u.subscribe
-              ? <button onClick={() => {props.unsubscribe(u.id)}}>Unsubscribe</button>
-              : <button onClick={() => {props.subscribe(u.id)}}>Subscribe</button>}
+              ? <button onClick={() => {
+                props.unsubscribe(u.id)
+              }}>Unsubscribe</button>
+              : <button onClick={() => {
+                props.subscribe(u.id)
+              }}>Subscribe</button>}
           </div>
         </span>
         <span>
